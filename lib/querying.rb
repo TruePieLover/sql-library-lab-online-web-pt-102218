52d0b18481a67a9b0a0292565151f9024adf3334
Brandon Teich <brandonteich123@gmail.com>
2019-01-29 01:13:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,12 +15,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"	  "SELECT species, COUNT(species)
-   FROM characters 
-   GROUP BY species 
-   ORDER BY COUNT(species) DESC LIMIT 1
-   "
-end	
+  "SELECT books.title, books.year FROM books WHERE series_id = 1;"
+end
 
 def select_name_and_series_subgenres_of_authors
 "SELECT authors.name, subgenres.name 
